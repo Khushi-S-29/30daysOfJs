74ff685f8b8f62f8701fb05aa1c3311b264ea47e
Khushi <khushiumalakshmi@gmail.com>
2024-07-20 16:53:46 +0530

    modified day02.js

--- day02.js
+++ day02.js
@@ -5,48 +5,49 @@
 let num1 = 17;
 let num2 = 29;
 // Task 1
-console.log(` ${num1} + ${num2} = ${num1+num2}`); 
+console.log(`${num1} + ${num2} = ${num1+num2}`); //Output : 17 + 29 = 46
 //Task 2
-console.log(`${num1} - ${num2} = ${num1-num2}`);
+console.log(`${num1} - ${num2} = ${num1-num2}`); //Output : 17 - 29 = -12
 //Task 3
-console.log(`${num1} * ${num2} = ${num1*num2}`);  
+console.log(`${num1} * ${num2} = ${num1*num2}`); //Output : 17 * 29 = 493
 //Task 4
-console.log(`${num1} / ${num2} = ${num1/num2}`);  
+console.log(`${num1} / ${num2} = ${num1/num2}`);  //Output : 17 / 29 = 0.5862068965517241
 //Task 5
-console.log(`${num1} % ${num2} = ${num1%num2}`);         
+console.log(`${num1} % ${num2} = ${num1%num2}`);  //Output : 17 % 29 = 17
 
 //Activity 2: ASSIGNMENT OPERATORS
+let num = 20
 let factor = 10
-//task 6
+//Task 6
 num1 += factor ;
-console.log("New number = ",num1);     
-//task 7
+console.log("New number = ",num1);     //Output : New number =  27
+//Task 7
 num1 -= factor;
-console.log(" num1 = ", num1);               
+console.log("num1 = ", num1);       //Output : num1 =  17  
 
 //Activity 3: COMAPARISON OPERATORS
-// task 8
-console.log(`Is ${num1} > ${num2} ?  ${num1>num2}`);   
-console.log(`Is ${num1} < ${num2} ?  ${num1<num2}`);   
+//Task 8
+console.log(`Is ${num1} > ${num2} ?  ${num1>num2}`);  //Output : Is 17 > 29 ?  false
+console.log(`Is ${num1} < ${num2} ?  ${num1<num2}`);  //Output : Is 17 < 29 ?  true
 
-//task 9
-console.log(`Is 10 <= 10 ? ${10<=10}`);            
-console.log(`Is 10 >= 9 ?  ${10>=9}`);  
-// task 10
-console.log("Is null==undefined ? ",null==undefined);
-console.log("Is null===undefined ? ",null===undefined);
+//Task 9
+console.log(`Is 10 <= 10 ? ${10<=10}`);  //Output : Is 17 > 29 ?  Is 10 <= 10 ? true       
+console.log(`Is A >= B ?  ${"A">="B"}`);  //Output : Is A >= B ?  false
+// Task 10
+console.log("Is null==undefined ? ", null==undefined); //Output : Is null==undefined ?  true
+console.log("Is null===undefined ? ", null===undefined); // Output : Is null===undefined ?  false
 
 //Activity 4: LOGICAL OPERATORS
-//task 11
-console.log(`true and false =  ${true && false}`)          
-//task 12
-console.log(`true or false =  ${true || false}`) 
-//task 13
-console.log(`!true =  ${!true}`);    
+//Task 11
+console.log(`true and false =  ${true && false}`)    //Output : true and false =  false  
+//Task 12
+console.log(`true or false =  ${true || false}`) //Output : true or false =  true
+//Task 13
+console.log(`!true =  ${!true}`);    //Output : !true = false
 
 //Activity 5: TERNARY OPERATOR 
-//task 14
-num1 > 0 ?  console.log(num1 ,"is a positive number"):console.log( num1 , "is a negative number");
+//Task 14
+num1 > 0 ?  console.log(num1 ,"is a positive number"):console.log( num1 , "is a negative number"); //Output : 17 is a positive number
 
 
 
