e8dd3431f087af2dc09cb4690a02181cd0e7db4c
Khushi <khushiumalakshmi@gmail.com>
2024-07-20 16:35:20 +0530

    added day02.js file with tasks pertaining to JS operators

---
+++ day02.js
@@ -0,0 +1,58 @@
+// DAY 2: OPERATORS
+
+//Activity 1: ARITHMETIC OPERATIONS
+
+let num1 = 17;
+let num2 = 29;
+// Task 1
+console.log(` ${num1} + ${num2} = ${num1+num2}`); 
+//Task 2
+console.log(`${num1} - ${num2} = ${num1-num2}`);
+//Task 3
+console.log(`${num1} * ${num2} = ${num1*num2}`);  
+//Task 4
+console.log(`${num1} / ${num2} = ${num1/num2}`);  
+//Task 5
+console.log(`${num1} % ${num2} = ${num1%num2}`);         
+
+//Activity 2: ASSIGNMENT OPERATORS
+let factor = 10
+//task 6
+num1 += factor ;
+console.log("New number = ",num1);     
+//task 7
+num1 -= factor;
+console.log(" num1 = ", num1);               
+
+//Activity 3: COMAPARISON OPERATORS
+// task 8
+console.log(`Is ${num1} > ${num2} ?  ${num1>num2}`);   
+console.log(`Is ${num1} < ${num2} ?  ${num1<num2}`);   
+
+//task 9
+console.log(`Is 10 <= 10 ? ${10<=10}`);            
+console.log(`Is 10 >= 9 ?  ${10>=9}`);  
+// task 10
+console.log("Is null==undefined ? ",null==undefined);
+console.log("Is null===undefined ? ",null===undefined);
+
+//Activity 4: LOGICAL OPERATORS
+//task 11
+console.log(`true and false =  ${true && false}`)          
+//task 12
+console.log(`true or false =  ${true || false}`) 
+//task 13
+console.log(`!true =  ${!true}`);    
+
+//Activity 5: TERNARY OPERATOR 
+//task 14
+num1 > 0 ?  console.log(num1 ,"is a positive number"):console.log( num1 , "is a negative number");
+
+
+
+
+
+
+
+
+
